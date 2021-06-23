4f256965185be3d77d3135e37452c5df269cdb4f
killbizz <45452791+killbizz@users.noreply.github.com>
2021-06-23 11:41:11 +0200

    add project

---
+++ src/main/java/com/example/webapp/PostVisualizer/PostVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp.PostVisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostVisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webapp/PostVisualizer/PostVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapp.PostVisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
